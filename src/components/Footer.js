7f95e682bc69caa9c55a6e68e3f3d9379bdb62a6
junnyang <lhr8440@gmail.com>
2025-04-03 01:25:46 +0900

    style: footer 스타일 수저

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+import "./styles/Footer.css";
+
 import logo from "../assets/JJ_logo.png";
 import insta from "../assets/Instagram.jpg";
 
@@ -9,17 +11,17 @@ function Footer() {
       <div className="Footer">
         <div className="footer_left"></div>
         <div className="footer_right">
-          <span className="footer_contents">주니어 쥬빌리</span>
-          <span className="footer_contents">
+          <div className="footer_contents">주니어 쥬빌리</div>
+          <div className="footer_contents">
             연락처: 010-3161-8383 | 메일 JuniorJubilee2015@gmail.com
-          </span>
+          </div>
           <div
             className="insta"
             onClick={() =>
               window.open("https://www.instagram.com/juniorjubilee__/")
             }
           >
-            <img src={insta} alt="instagram"></img>
+            <img className="insta_logo" src={insta} alt="instagram"></img>
             <span>instagram</span>
           </div>
         </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+
+import "./styles/Main.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -6,7 +8,9 @@ function Main() {
   return (
     <div className="wrap">
       <Header></Header>
-      <div className="contents"></div>
+      <div className="contents">
+        <div>안녕</div>
+      </div>
       <Footer></Footer>
     </div>
   );
