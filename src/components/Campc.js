123d69ee382d08e540db37a1d7d98f2497ecff1a
junnyang <lhr8440@gmail.com>
2025-04-09 23:19:56 +0900

    feat: camp 완성

--- src/components/Campc.js
+++ src/components/Campc.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 import Poster from "../assets/24 poster.svg";
+import "./styles/Campc.css";
 
 export class Camp extends Component {
   render() {
     return (
       <div className="camp">
-        <div className="camp-name"></div>
+        <div className="camp-name-div">
+          <div className="camp-name">2024</div>
+          <div className="camp-name">NEW BALANCE</div>
+        </div>
         <div className="camp-poster">
           <img className="poster" src={Poster} alt="poster" />
         </div>
