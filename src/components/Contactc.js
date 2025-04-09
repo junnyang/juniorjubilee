a9d7f008318acc3d783287b0a64c489b7c4bb388
junnyang <lhr8440@gmail.com>
2025-04-10 03:14:26 +0900

    feat: contact 생성 및 css 작업

---
+++ src/components/Contactc.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./styles/Contactc.css";
+
+export class Contactc extends Component {
+  render() {
+    return (
+      <div className="contact">
+        <div className="contact-head">CONTACT</div>
+        <div className="contact-cont">
+          <div className="contact-txt">
+            E-mail | JuniorJubilee2015@gmail.com
+          </div>
+          <div className="contact-txt">전화 | 031-906-0673</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Contactc;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Contactc from "../components/Contactc";
 
 function Contact() {
   return (
     <div className="wrap">
       <Header></Header>
+      <Contactc></Contactc>
+      <Footer></Footer>
     </div>
   );
 }
