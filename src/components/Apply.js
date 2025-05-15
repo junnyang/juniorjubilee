0132f55ff28779ab0601435c7f5566504b11d27d
junnyang <lhr8440@gmail.com>
2025-05-15 13:37:09 +0900

    feat: 신청 링크 변경

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -7,14 +7,21 @@ export class Apply extends Component {
   render() {
     return (
       <div className="applys">
-        <div className="apply" onClick={() => alert`신청기간이 아닙니다.`}>
+        <div
+          className="apply"
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSfV5MsxeynqIQ0UfcxV0UElxtZKXrubMC0vm8fNZCLy_EXjSQ/viewform"
+            )
+          }
+        >
           2025 캠프 학생으로 참여하기
         </div>
         <div
           className="apply"
           onClick={() =>
             window.open(
-              "https://docs.google.com/forms/d/e/1FAIpQLSd8bT-7EbI1-HJWtO3m1pyBpkEp5GPuTN4EDPwsU5VKFjqTHw/viewform"
+              "https://docs.google.com/forms/d/e/1FAIpQLSfV5MsxeynqIQ0UfcxV0UElxtZKXrubMC0vm8fNZCLy_EXjSQ/viewform"
             )
           }
         >
